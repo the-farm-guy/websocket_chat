9f43bd39d5e6ec17baf388dc8a3dabdb803d0a4e
the-farm-guy <thefarmguy011@gmail.com>
2025-04-26 11:39:50 +0000

    initial commit

---
+++ client.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Cloning the repository..."
+git clone https://github.com/your-username/your-repo-name.git
+cd your-repo-name || { echo "Failed to enter the repository directory."; exit 1; }
+
+echo "Creating a Python virtual environment..."
+python3 -m venv venv
+
+echo "Activating the virtual environment..."
+source venv/bin/activate
+
+echo "Installing dependencies..."
+pip install -r requirments.txt
+
+echo "Navigating the directory"
+cd group_chat || { echo "Failed to navigate to the group_chat directory."; exit 1; }
+
+echo "Running the application"
+python3 client_server.py
+
+echo "Script completed."
