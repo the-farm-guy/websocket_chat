c41a877c190e5d4b49df8297e8ac87715a9f6660
the-farm-guy <thefarmguy011@gmail.com>
2025-04-26 11:46:29 +0000

    initial commit

--- client.sh
+++ client.sh
@@ -17,6 +17,6 @@ echo "Navigating the directory"
 cd group_chat || { echo "Failed to navigate to the group_chat directory."; exit 1; }
 
 echo "Running the application"
-python3 client_server.py
+python3 clients_server.py
 
 echo "Script completed."
